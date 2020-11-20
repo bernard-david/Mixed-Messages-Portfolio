80a11aa59ed79c77b173f59cb97bf57b2de5dc68
bernard-david <dpbernard18@gmail.com>
2020-11-20 13:28:50 -0500

    Create array for final message

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Creates a Random number
 function createRandomNumber(num) {
     return Math.floor(Math.random() * num)
-} 
+}; 
 
+// Stores messages that will randomly generate in the program. 
 const messageData = {
     verb: ['fart', 'whistle', 'kick', 'sleep', 'wash', 'drink'],
     noun: ['bear', 'mirror', 'toe', 'tea cup', 'flower', 'hat'],
     ingVerb: ['running', 'spitting', 'clucking', 'burping', 'spooning', 'cooking'],
     number: ['1', '54,377', '12', 'three million', '0.55', '36']
-}
+};
+
+// Stores final message result to be printed.
+let madLibsMessage = [];
