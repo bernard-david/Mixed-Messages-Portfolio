4cf4ac09e7b250d0cc28a64d7acc5c4f96ac0133
bernard-david <dpbernard18@gmail.com>
2020-11-20 13:57:37 -0500

    Create a for loop to iterate over the object and begin crafting final message.

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ const messageData = {
 
 // Stores final message result to be printed.
 let madLibsMessage = [];
+
+// Iterate over the object.
+for(let word in messageData) {
+    let randomIndex = createRandomNumber(messageData[word.length]);
+
+    // Take the object's properties to create a message and add it to the madLibsMessage array.
+    switch(word) {
+        case 'verb':
+            madLibsMessage.push(`They say that to properly ${messageData[word][randomIndex]} a `)
+            break;
+        case 'noun':
+            madLibsMessage.push(`${messageData[word][randomIndex]} you must first `)
+            break;
+        case 'ingVerb':
+            madLibsMessage.push(`begin by ${messageData[word][randomIndex]} it. `)
+            break;
+        case 'number':
+            madLibsMessage.push(`Well, I must say that I tried this ${messageData[word][randomIndex]} time(s). `)
+            break;
+        
+    }
+}
