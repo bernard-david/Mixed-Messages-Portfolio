f706c502dc8d5e555bb240776c4752dbf68f5734
bernard-david <dpbernard18@gmail.com>
2020-11-20 13:07:14 -0500

    Stored Message Data

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('This is a test')
+const messageData = {
+    verb: ['fart', 'whistle', 'kick', 'sleep', 'wash', 'drink'],
+    noun: ['bear', 'mirror', 'toe', 'tea cup', 'flower', 'hat'],
+    ingVerb: ['running', 'spitting', 'clucking', 'burping', 'spooning', 'cooking'],
+    number: ['1', '54,377', '12', 'three million', '0.55', '36']
+}
